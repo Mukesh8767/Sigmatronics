800ea75064c233181d4bf98016e76ae31c0921c3
Mukesh Paliwal <138951985+Mukesh8767@users.noreply.github.com>
2025-08-06 22:57:40 +0530

    Update axiosInstance.ts

--- frontend/utils/axiosInstance.ts
+++ frontend/utils/axiosInstance.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
    baseURL: 'https://16-170-250-207.nip.io/api',
  // baseURL:'http://localhost:5000',
+   
   withCredentials: true,
 });
 
