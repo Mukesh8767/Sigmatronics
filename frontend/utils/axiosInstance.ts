a1abea81b25a6974ad3372c6ea2dc6e2c3b4dd60
Mukesh Paliwal <138951985+Mukesh8767@users.noreply.github.com>
2025-11-09 19:11:32 +0530

    Update axios baseURL for API requests

--- frontend/utils/axiosInstance.ts
+++ frontend/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://16-170-250-207.nip.io/api',
+  baseURL: 'https://16-170-250-207.nip.io',
   
 //baseURL:'http://localhost:5000',
 
