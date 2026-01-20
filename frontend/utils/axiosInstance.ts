d74f38ab3b6bdb7f27a4099c077a966edf03663f
Mukesh Paliwal <138951985+Mukesh8767@users.noreply.github.com>
2026-01-20 18:15:49 +0530

    Update axios base URL to new API endpoint

--- frontend/utils/axiosInstance.ts
+++ frontend/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://16-170-250-207.nip.io/api',
+  baseURL: 'https://qzfyp43wu5.execute-api.us-east-1.amazonaws.com',
     // baseURL:'http://localhost:5000',
 
  
